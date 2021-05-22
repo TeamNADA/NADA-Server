b58ff8706c7a11566e635ecb0abe0b469df0da7f
yaeoni <tjghwnd103@gmail.com>
2021-05-22 22:54:33 +0900

    initial structure.

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+
+const app = express();
+import connectDB from "./Loaders/db";
+
+//connect DB
+connectDB();
+
+app.use(express.json());
+
+// define route
+//app.use("/api/users", require("./api/users"));
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "production" ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
+
+  app
+  .listen(5000, () => {
+    console.log(`
+    ################################################
+    🛡️  Server listening on port: 5000 🛡️
+    ################################################
+  `);
+  })
+  .on("error", (err) => {
+    console.error(err);
+    process.exit(1);
+  });
